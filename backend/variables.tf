c059c90ec0456c260330064abb27e98ae58c8b26
Jubril Adeyi <jadeyi@gmail.com>
2023-05-08 21:45:20 +0100

    commit2

--- backend/variables.tf
+++ backend/variables.tf
@@ -1,32 +1,11 @@
-# variable "access_key" {
-#   type    = string
-#   default = "AKIA2Q3K5UNN67ZRIUEP"
-# }
-# variable "secret_key" {
-#   type    = string
-#   default = "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"
-# }
-# variable "region" {
-#   type    = string
-#   default = "us-east-1"
-# }
-# variable "bucket_name" {
-#   type    = string
-#   default = "terra-tfstate-2228"
-# }
-# variable "dynamo-db-name" {
-#   type    = string
-#   default = "terra-statelock-2228"
-# }
-
 
 variable "access_key" {
   type    = string
-  default = "AKIAROHNP6JV3FKZUROW"
+  default = ""
 }
 variable "secret_key" {
   type    = string
-  default = "8cfHLdYkrflHx4RGz5unME0q0EVDGGEjz9cl6RkV"
+  default = ""
 }
 variable "region" {
   type    = string
