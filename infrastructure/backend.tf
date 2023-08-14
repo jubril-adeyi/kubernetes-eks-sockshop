3e94db473dababa8312462fb1363b54e45715d9e
Jubril Adeyi <jadeyi@gmail.com>
2023-08-14 01:17:54 +0100

    modified jenkinsfile 2

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -2,10 +2,20 @@
 
 terraform {
   backend "s3" {
-    bucket         = var.bucket
-    key            = var.key
-    region         = var.region
-    dynamodb_table = var.dynamodb_table
+    bucket         = "terra-tfstate-2229"
+    key            = "global/infrastructure/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terra-statelock-2229"
     encrypt        = true
   }
 }
+
+# terraform {
+#   backend "s3" {
+#     bucket         = var.bucket
+#     key            = var.key
+#     region         = var.region
+#     dynamodb_table = var.dynamodb_table
+#     encrypt        = true
+#   }
+# }
